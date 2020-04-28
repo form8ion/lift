05c420bda5155f067f0512608bbeb37bae75b09a
Matt Travi <programmer@travi.org>
2020-04-27 22:53:49 -0500

    feat(output): provided output about updating the docs

--- src/documentation.js
+++ src/documentation.js
@@ -1,9 +1,12 @@
 import fs from 'fs';
+import {info} from '@travi/cli-messages';
 import badgeInjectorPlugin from '@form8ion/remark-inject-badges';
 import updateLegacyBadgeMarkers from '@form8ion/remark-update-legacy-badge-markers';
 import remark from '../thirdparty-wrappers/remark';
 
 export default function ({projectRoot, results}) {
+  info('Lifting Documentation');
+
   const pathToReadme = `${projectRoot}/README.md`;
 
   return new Promise((resolve, reject) => {
