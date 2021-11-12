bf9f9ffce4fc36afddffdc4453914fa876cb9983
Matt Travi <programmer@travi.org>
2021-11-12 11:36:14 -0600

    feat(vcs): determined host from the git details of the existing project

--- src/vcs-test.js
+++ src/vcs-test.js
@@ -22,14 +22,15 @@ suite('vcs', () => {
     test('that the existing details are determined from the remote origin', async () => {
       const owner = any.word();
       const name = any.word();
+      const host = any.word();
       const remoteUrl = any.url();
       const repository = any.simpleObject();
       const projectRoot = any.string();
       Repository.open.withArgs(projectRoot).resolves(repository);
       Remote.lookup.withArgs(repository, 'origin').resolves({url: () => remoteUrl});
-      hostedGitInfo.fromUrl.withArgs(remoteUrl).returns({user: owner, project: name});
+      hostedGitInfo.fromUrl.withArgs(remoteUrl).returns({user: owner, project: name, type: host});
 
-      assert.deepEqual(await determineExistingHostDetails({projectRoot}), {owner, name});
+      assert.deepEqual(await determineExistingHostDetails({projectRoot}), {owner, name, host});
     });
   });
 });
--- src/vcs.js
+++ src/vcs.js
@@ -3,7 +3,7 @@ import hostedGitInfo from 'hosted-git-info';
 
 export async function determineExistingHostDetails({projectRoot}) {
   const remoteOrigin = await Remote.lookup(await Repository.open(projectRoot), 'origin');
-  const {user: owner, project: name} = hostedGitInfo.fromUrl(remoteOrigin.url());
+  const {user: owner, project: name, type: host} = hostedGitInfo.fromUrl(remoteOrigin.url());
 
-  return {owner, name};
+  return {owner, name, host};
 }
