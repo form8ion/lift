2b872a52701cb3cc08cc993fed18b0b4d7c70d1d
Matt Travi <programmer@travi.org>
2023-07-15 15:27:49 -0500

    build(rollup): stopped using the common-js plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import autoExternal from 'rollup-plugin-auto-external';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal(), commonjs()],
+  plugins: [autoExternal()],
   output: [
     {file: 'lib/index.js', format: 'esm', sourcemap: true}
   ]
