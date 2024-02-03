1da49237531eb6968314fc12375009d16baf8ad0
Matt Travi <programmer@travi.org>
2024-02-02 18:24:21 -0600

    refactor(git-parse-url): imported as a verb

--- src/vcs.js
+++ src/vcs.js
@@ -1,10 +1,10 @@
 import {simpleGit} from 'simple-git';
-import GitUrlParse from 'git-url-parse';
+import parseGitUrl from 'git-url-parse';
 
 export async function determineExistingHostDetails({projectRoot}) {
   const git = simpleGit(projectRoot);
   const remoteOrigin = await git.remote(['get-url', 'origin']);
-  const {user: owner, project: name, type: host} = GitUrlParse(remoteOrigin);
+  const {user: owner, project: name, type: host} = parseGitUrl(remoteOrigin);
 
   return {owner, name, host};
 }
