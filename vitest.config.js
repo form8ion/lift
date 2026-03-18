13a18df6a439263235f6f5b848bdd53964570927
Matt Travi <programmer@travi.org>
2026-03-18 17:26:19 -0500

    test(vitest): limit coverage collection to the src/ directory
    and exclude manifest files

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,12 @@
 import {defineConfig} from 'vitest/config';
 
-export default defineConfig({test: {restoreMocks: true}});
+export default defineConfig({
+  test: {
+    restoreMocks: true,
+    coverage: {
+      provider: 'v8',
+      include: ['src/**'],
+      exclude: ['src/**/index.js']
+    }
+  }
+});
