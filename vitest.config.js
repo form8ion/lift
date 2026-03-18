7abbfec981bfa9ef63bc22fd16b0249b9bba5b1b
Matt Travi <programmer@travi.org>
2026-03-18 17:27:51 -0500

    test(vitest): add further test bleed prevention config

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,8 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     restoreMocks: true,
+    mockReset: true,
+    resetEnv: true,
     coverage: {
       provider: 'v8',
       include: ['src/**'],
